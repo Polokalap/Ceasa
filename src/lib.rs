b481990d41c4abf5f0975e90e2d923cca19cd488
polokalap <Polokalap@autizm.us>
2025-03-13 22:32:43 +0000

    Adding randint

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 pub fn read() -> String {
     let mut input = String::new();
@@ -6,5 +7,11 @@ pub fn read() -> String {
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
     input.trim().to_string()
+
+}
+
+pub fn randint(start: i32, end: i32) -> i32 {
+
+    rand::thread_rng().gen_range(start..=end)
 
 }
